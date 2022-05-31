ba87a71e2284628df5de00dd5472a5f82399b520
Gary Yau <gary.yau@hotmail.com>
2022-05-31 21:30:23 +0900

    Added javascript to change image

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 particlesJS.load('particles-js', 'particle-cfg.json', function() {
         console.log('callback - particles.js config loaded');
         });
+
+let ice = document.querySelector(".fas.fa-icicles");
+
+ice.addEventListener('click', function handleClick() {
+  (document.querySelector(".img-circle").src="./images/ice-man.png");
+});
